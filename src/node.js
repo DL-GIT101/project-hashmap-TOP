c3a9afe424672b762b4f580e3fa5469ac42172cc
donlHenard <pastor.git101@gmail.com>
2024-03-28 18:01:49 +0800

    add key property to node

--- src/node.js
+++ src/node.js
@@ -1,9 +1,12 @@
-const CreateNode = (value = null) => {
+const CreateNode = ( key, value ) => {
     
-    let _value = value;
-    let _next = null;
+    let _key     = key;
+    let _value  = value;
+    let _next   = null;
     
     return {
+        getKey: () => _key,
+        setKey: (key) => _key = key,
         getValue: () => _value,
         setValue: (value) => _value = value,
         getNext: () => _next,
