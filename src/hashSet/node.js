b1f3a7717d329bd02c6c6059bb3ffff3bb68eea5
donlHenard <pastor.git101@gmail.com>
2024-03-29 18:37:02 +0800

    remove value property

--- src/hashSet/node.js
+++ src/hashSet/node.js
@@ -1,14 +1,11 @@
-const CreateNode = ( key, value ) => {
+const CreateNode = ( key ) => {
     
-    let _key     = key;
-    let _value  = value;
+    let _key    = key;
     let _next   = null;
     
     return {
         getKey: () => _key,
         setKey: (key) => _key = key,
-        getValue: () => _value,
-        setValue: (value) => _value = value,
         getNext: () => _next,
         setNext: (node) => _next = node,
     } 
